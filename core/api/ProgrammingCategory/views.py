b9bc65e33717d8a1ee5db87265823afab4b98e06
DjamalovMuhammadAli <djamalov.muhammad.ali7@gmail.com>
2023-01-10 16:59:12 +0500

    .

--- core/api/ProgrammingCategory/views.py
+++ core/api/ProgrammingCategory/views.py
@@ -6,12 +6,11 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
 from ProgrammingCategory.forms import *
 from ProgrammingCategory.models import *
-from ProgrammingCategory.utils import *
 from django.views.generic.edit import FormView
-
+from django.views.generic import ListView, DetailView, CreateView
+from ProgrammingCategory.utils import *
 
 class DeveloperHome(DataMixin, ListView):
   model = Developer
